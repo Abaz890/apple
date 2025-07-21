148c35b4bc67f9e1279261f1baf65198bdd48349
Caz <abaz77694@gmail.com>
2025-07-21 15:56:27 +0500

    Add HeroSection to IndexPage

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="absolute bottom-0 left-0 w-full p-4 text-white text-center z-10">
+    <footer className="relative w-full p-4 text-white text-center z-10 bg-black">
       <p>&copy; {new Date().getFullYear()} Your Brand. All rights reserved.</p>
     </footer>
   );
--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -6,19 +6,25 @@ import HeroSection from '../components/HeroSection';
 
 const IndexPage = () => {
   return (
-    <div className='w-full h-screen relative'>
-      <div className="absolute flex flex-col items-center text-white top-32 left-1/2 -translate-x-1/2 font-sans z-10"> 
-        <h3 className='masked text-7xl tracking-tighter font-[650]'>macbook pro.</h3>
-        <h5>Oh so pro! </h5>
-        <p className='text-center w-4/5'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. list-group-iteml list-group-item </p>
-      </div>
+    <div className='w-full'>
+      {/* Hero Section with Video */}
+      <HeroSection />
+      
+      {/* 3D MacBook Section */}
+      <div className='w-full h-screen relative'>
+        <div className="absolute flex flex-col items-center text-white top-32 left-1/2 -translate-x-1/2 font-sans z-10"> 
+          <h3 className='masked text-7xl tracking-tighter font-[650]'>macbook pro.</h3>
+          <h5>Oh so pro! </h5>
+          <p className='text-center w-4/5'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. list-group-iteml list-group-item </p>
+        </div>
 
-      <Canvas camera={{ fov: 12, position: [0, -10, 220] }} className="absolute inset-0 z-20">
-        <Environment files={['https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_08_4k.exr']} />
-        <ScrollControls pages={3}>
-          <MacContainer />
-        </ScrollControls>
-      </Canvas>
+        <Canvas camera={{ fov: 12, position: [0, -10, 220] }} className="absolute inset-0 z-20">
+          <Environment files={['https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_08_4k.exr']} />
+          <ScrollControls pages={3}>
+            <MacContainer />
+          </ScrollControls>
+        </Canvas>
+      </div>
     </div>
   );
 };
