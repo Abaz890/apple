d71847195eeeb66b846d27c30b8a0070d7e75302
Caz <abaz77694@gmail.com>
2025-07-21 15:57:07 +0500

    Fix double scrollbar by removing duplicate HeroSection

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -2,14 +2,10 @@ import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Environment, ScrollControls } from '@react-three/drei';
 import MacContainer from '../components/MacContainer'; 
-import HeroSection from '../components/HeroSection';
 
 const IndexPage = () => {
   return (
     <div className='w-full'>
-      {/* Hero Section with Video */}
-      <HeroSection />
-      
       {/* 3D MacBook Section */}
       <div className='w-full h-screen relative'>
         <div className="absolute flex flex-col items-center text-white top-32 left-1/2 -translate-x-1/2 font-sans z-10"> 
