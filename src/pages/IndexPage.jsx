bf724832a23a90ac6669445a7de9e664a7d050cf
algodevone@gmail.com <algodevone@gmail.com>
2025-07-22 10:59:48 +0500

    New_Commit

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Environment, ScrollControls } from '@react-three/drei';
 import MacContainer from '../components/MacContainer'; 
-import HeroSection from '../components/HeroSection';
 
 const IndexPage = () => {
   return (
     <div className='w-full'>
-      {/* 3D MacBook Section */}
       <div className='w-full h-screen relative'>
         <div className="absolute flex flex-col items-center text-white top-32 left-1/2 -translate-x-1/2 font-sans z-10"> 
           <h3 className='masked text-7xl tracking-tighter font-[650]'>macbook pro.</h3>
@@ -22,7 +20,6 @@ const IndexPage = () => {
           </ScrollControls>
         </Canvas>
       </div>
-      <HeroSection/>
     </div>
   );
 };
